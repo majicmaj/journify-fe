ee2e45a19669d9222e1b6a3dea06e6ff8934dc6c
majicmaj <majd.tarbin@outlook.com>
2024-05-25 13:56:06 -0400

    Refactor Text component for simplicity

--- src/components/display/Text.tsx
+++ src/components/display/Text.tsx
@@ -1,31 +1,22 @@
 import { Typography, TypographySystem } from "@mui/joy";
 
+type THeading = 1 | 2 | 3 | 4 | undefined;
+type TSize = "sm" | "md" | "lg";
+type TLevel = keyof TypographySystem;
 interface IText {
   children?: React.ReactNode;
-  size?: "sm" | "md" | "lg";
+  h?: THeading;
+  size?: TSize;
   title?: boolean;
-  h?: 1 | 2 | 3 | 4 | undefined;
 }
 
-const getLevel = (h: number | undefined, size: string, title: boolean) => {
-  if (h) {
-    return `h${h}`;
-  }
-  let level = "";
-
-  if (title) {
-    level = "title-";
-  } else {
-    level = "body-";
-  }
-
-  level += size;
-
-  return level;
+const getLevel = (h: THeading, size: string, title: boolean) => {
+  if (h) return `h${h}`; // h1, h2, h3, h4
+  return (title ? "title-" : "body-") + size; // title-sm, title-md, title-lg, body-sm, body-md, body-lg
 };
 
 const Text = ({ children, size = "md", title = false, h }: IText) => {
-  const level = getLevel(h, size, title) as keyof TypographySystem;
+  const level = getLevel(h, size, title) as TLevel;
 
   return <Typography level={level}>{children}</Typography>;
 };
