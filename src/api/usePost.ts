0d9c2d72a819cf02c8571ef287dcc758331a538b
majicmaj <majd.tarbin@outlook.com>
2024-05-25 19:25:01 -0400

    Delete unused usePost file

--- src/api/usePost.ts
+++
@@ -1,8 +0,0 @@
-const usePost = (url: string) => {
-  const post = (body: unknown) =>
-    window.localStorage.setItem(url, JSON.stringify(body));
-
-  return { post };
-};
-
-export default usePost;
