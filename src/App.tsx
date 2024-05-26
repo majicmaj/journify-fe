88c02117344861a989b42cbf75e4c710ffab9c0c
majicmaj <majd.tarbin@outlook.com>
2024-05-25 20:20:17 -0400

    Utilize get and post hooks for journals in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,63 @@
 import { SendRounded } from "@mui/icons-material";
-import { Container, Divider, IconButton, Textarea } from "@mui/joy";
+import { Container, IconButton, Textarea } from "@mui/joy";
 import { useState } from "react";
-import usePost from "./api/usePost";
+import useGetJournals from "./api/journal/useGetJournals";
+import usePostJournal from "./api/journal/usePostJournal";
+import { IJournal } from "./api/journals";
 import Text from "./components/display/Text";
+import Journal from "./components/journal/Journal";
 
 function App() {
   const [journal, setJournal] = useState<string[]>([]);
 
   const timestamp = new Date().toLocaleString();
-  const { post } = usePost(`/api/journal`);
+
+  const { data: journals, refetch } = useGetJournals();
+  const { mutate: post } = usePostJournal();
+
+  const isJournals = journals?.length > 0;
 
   const handleSubmit = (text: string) => {
-    post({ text, timestamp });
-    window.location.reload();
+    post({ timestamp, text });
+    refetch();
   };
 
-  const item = localStorage.getItem("/api/journal");
-  const parsedItem = item ? JSON.parse(item) : {};
   return (
     <Container>
-      <div className="py-1">
-        <Text h={1}>Journify</Text>
-      </div>
+      <div className="h-screen">
+        <div className="py-2">
+          <Text h={1}>Journify</Text>
+        </div>
 
-      <div>
-        {parsedItem ? (
-          <div>
-            <Text>{parsedItem.text || "N/A"}</Text>
-            <Text size="sm">{parsedItem.timestamp}</Text>
+        {isJournals && (
+          <div className="h-full flex flex-col gap-2">
+            {journals?.map((journal: IJournal) => (
+              <Journal journal={journal} key={journal.timestamp} />
+            ))}
           </div>
-        ) : (
-          <Text>No entries yet.</Text>
         )}
-      </div>
-      <Divider />
-      <div className="absolute bottom-0 left-0 right-0 p-4 flex items-center gap-2 border-t">
-        <Textarea
-          placeholder="Type something..."
-          maxRows={6}
-          onChange={(e) => setJournal(e.target.value.split("\n"))}
-          onKeyPress={(e) =>
-            e.key === "Enter" && handleSubmit(e.currentTarget.value)
-          }
-          value={journal.join("\n")}
-          sx={{ flexGrow: 1 }}
-        />
-        <IconButton size="lg" onClick={() => handleSubmit(journal.join("\n"))}>
-          <SendRounded />
-        </IconButton>
+
+        {!isJournals && (
+          <div className="h-full grid place-items-center">
+            <Text>No journals yet</Text>
+          </div>
+        )}
+
+        <div className="sticky bg-white bottom-0 left-0 right-0 p-4 flex items-center gap-2">
+          <Textarea
+            placeholder="Type something..."
+            maxRows={6}
+            onChange={(e) => setJournal(e.target.value.split("\n"))}
+            value={journal.join("\n")}
+            sx={{ flexGrow: 1 }}
+          />
+          <IconButton
+            size="lg"
+            onClick={() => handleSubmit(journal.join("\n"))}
+          >
+            <SendRounded />
+          </IconButton>
+        </div>
       </div>
     </Container>
   );
