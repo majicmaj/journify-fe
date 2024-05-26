8c62bf3bd4d7221b0790501873e68973992acb5e
majicmaj <majd.tarbin@outlook.com>
2024-05-25 20:38:17 -0400

    Add usePutJournal hook

---
+++ src/api/journal/usePutJournal.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { IJournal, putJournal } from "../journals";
+
+const usePutJournal = () => {
+  const queryClient = useQueryClient();
+
+  const handlePut = async (journal: IJournal) => {
+    putJournal(journal);
+  };
+
+  const mutation = useMutation({
+    mutationKey: ["journal_put"],
+    mutationFn: handlePut,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["journals"],
+      });
+    },
+  });
+
+  return mutation;
+};
+
+export default usePutJournal;
