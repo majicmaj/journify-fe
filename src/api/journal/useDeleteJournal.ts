e4ef2fc60370c114d873a03445603b354a06b1ff
majicmaj <majd.tarbin@outlook.com>
2024-05-25 20:13:20 -0400

    Add useDeleteJournal hook

---
+++ src/api/journal/useDeleteJournal.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteJournal } from "../journals";
+
+const useDeleteJournal = () => {
+  const queryClient = useQueryClient();
+
+  const handleDelete = async (timestamp: string) => {
+    deleteJournal(timestamp);
+  };
+
+  const mutation = useMutation({
+    mutationKey: ["journal_delete"],
+    mutationFn: handleDelete,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["journals"],
+      });
+    },
+  });
+
+  return mutation;
+};
+
+export default useDeleteJournal;
