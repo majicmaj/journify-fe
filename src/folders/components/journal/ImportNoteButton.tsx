acb96cc04cbf52e796d183909d502afd21f73c83
Jose Sanchez <engineer@copydataai.com>
2024-06-07 14:30:50 +0100

    feat(components): add ImportNoteButton

---
+++ src/folders/components/journal/ImportNoteButton.tsx
@@ -0,0 +1,47 @@
+import { PublishRounded, BorderColorRounded } from "@mui/icons-material";
+import { IconButton, Button } from "@mui/joy";
+
+import { useState } from "react";
+
+interface IImportNoteButton {
+  fullButton?: boolean;
+  isImportOpenModal: boolean;
+  setIsImportOpenModal: (value: boolean) => void;
+}
+
+const ImportNoteButton = ({
+  fullButton,
+  isImportOpenModal,
+  setIsImportOpenModal,
+}: IImportNoteButton) => {
+  if (fullButton) {
+    return (
+      <Button
+        onClick={() => setIsImportOpenModal(true)}
+        startDecorator={<BorderColorRounded />}
+        fullWidth
+        size="lg"
+        variant="solid"
+        sx={{
+          width: "calc(100vw - 1rem)",
+        }}
+      >
+        Import
+      </Button>
+    );
+  }
+
+  return (
+    <IconButton
+      onClick={() => setIsImportOpenModal(true)}
+      size="lg"
+      color="primary"
+      variant="solid"
+      sx={{ borderRadius: "100%" }}
+    >
+      <PublishRounded />
+    </IconButton>
+  );
+};
+
+export default ImportNoteButton;
